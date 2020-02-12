8dae62f37ca4999ed13492bc99e1af4b458e8334
Joe Bu <buzehao@163.com>
2020-02-12 14:46:22 +0800

    Add database support with flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,10 +1,70 @@
 # -*- coding:utf-8 -*-
 # @auhtor: Joe-Bu
 
+import os
+import click
+
 from flask import Flask
 from flask import url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + \
+    os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+def forge():
+    '''
+        Generate fake data
+    '''
+    db.create_all()
+
+    name = "Joe Bu"
+    movies = [
+        {'title': '我不是药神', 'year': '2019'},
+        {'title': '流感', 'year': '2018'},
+        {'title': '铁线虫入侵', 'year': '2018'},
+        {'title': '娜扎', 'year': '2019'},
+        {'title': '神探狄仁杰', 'year': '2018'},
+        {'title': '为爱迫降', 'year': '2020'},
+        {'title': 'Silicon Vally', 'year': '2017'},
+        {'title': '战狼2', 'year': '2019'},
+        {'title': '亲爱的，新年好', 'year': '2020'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+
+    for movie in movies:
+        mv = Movie(title=movie['title'], year=movie['year'])
+        db.session.add(mv)
+
+    db.session.commit()
+    click.echo("Insert Done!")
+
+
+# tablename: user
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(28))
+
+
+# tablename: movie
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
 
 '''
 @app.route('/')
@@ -44,18 +104,8 @@ def test_url_for():
     return 'Test Page'
 
 
-name = 'Joe Bu'
-movies = [
-    {'title': 'Island', 'year': '2020'},
-    {'title': 'Newland', 'year': '2020'},
-    {'title': 'Myland', 'year': '2020'},
-    {'title': 'Swisland', 'year': '2020'},
-    {'title': 'Iceland', 'year': '2020'},
-    {'title': 'Pkland', 'year': '2020'},
-    {'title': 'Ourland', 'year': '2020'}
-]
-
-
 @app.route('/index')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
